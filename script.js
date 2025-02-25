1aa3247ace345b1a6cfedf56cf59bcfed0aee487
zevilgronzys <prashantkeshriit@gmail.com>
2025-02-25 22:31:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 function getThumbnail() {
-    let url = document.getElementById("videoUrl").value;
-    let videoId = extractVideoID(url);
+    let videoUrl = document.getElementById("videoUrl").value;
+    let videoId = extractVideoID(videoUrl);
+
     if (videoId) {
-        let thumbnailURL = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
-        document.getElementById("result").innerHTML = `
-            <img src="${thumbnailURL}" alt="Thumbnail">
-            <br>
-            <a href="${thumbnailURL}" download="thumbnail.jpg">
-                <button>Download Thumbnail</button>
-            </a>
-        `;
+        document.getElementById("thumbnailContainer").classList.remove("hidden");
+
+        // Different thumbnail sizes
+        let thumbnail4K = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+        let thumbnailHD = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
+        let thumbnailSD = `https://img.youtube.com/vi/${videoId}/mqdefault.jpg`;
+        let thumbnailLow = `https://img.youtube.com/vi/${videoId}/default.jpg`;
+
+        // Set the thumbnail image
+        document.getElementById("thumbnail").src = thumbnail4K;
+
+        // Set download links
+        document.getElementById("download4K").href = thumbnail4K;
+        document.getElementById("downloadHD").href = thumbnailHD;
+        document.getElementById("downloadSD").href = thumbnailSD;
+        document.getElementById("downloadLow").href = thumbnailLow;
     } else {
-        alert("Invalid YouTube URL!");
+        alert("Invalid YouTube URL. Please enter a correct one!");
     }
 }
 
 function extractVideoID(url) {
-    let regExp = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/;
-    let match = url.match(regExp);
+    let match = url.match(/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/);
     return match ? match[1] : null;
 }
